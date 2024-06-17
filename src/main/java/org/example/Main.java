97604cdafcae576c87690872d232f5d8fcef29ab
Vsevolod <pro.vsevolod64@gmail.com>
2024-06-17 18:04:05 +0200

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,30 @@
 package org.example;
 
+
 import CustomStructures.CustomArrayList;
-import Entities.*;
-
-import java.util.Arrays;
-
+import CustomStructures.CustomLinkedList;
+import CustomStructures.CustomStack;
 
 public class Main {
     public static void main(String[] args) {
-        CustomArrayList<Integer> custom = new CustomArrayList<>();
+
+        CustomStack<Integer> stack = new CustomStack<>();
+        CustomLinkedList<Integer> list = new CustomLinkedList<>();
+
+
+        stack.push(12);
+        stack.push(13);
+        stack.push(14);
+        stack.push(15);
+        stack.pop();
+
+
+        System.out.println(stack);
+
 
 
 
     }
+
+
 }
